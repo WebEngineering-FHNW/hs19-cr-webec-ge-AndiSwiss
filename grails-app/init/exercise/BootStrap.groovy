6c57d458e0d17db47806efaf6853f1d42dd073ea
Andreas Ambühl <36681000+AndiSwiss@users.noreply.github.com>
2019-12-09 17:28:57 +0100

    added dependency so that clearTime works again

--- grails-app/init/exercise/BootStrap.groovy
+++ grails-app/init/exercise/BootStrap.groovy
@@ -2,22 +2,22 @@ package exercise
 
 import grails.util.Environment
 
+import java.text.SimpleDateFormat
+
 class BootStrap {
 
     def init = { servletContext ->
-        if (Environment.current == Environment.PRODUCTION) {
-            return
-        }
+        Date today = new Date().clearTime()
+        def pattern = "yyyy-MM-dd"
+        Date aDate = new SimpleDateFormat(pattern).parse("2017-03-26").clearTime()
 
-        // TODO: .clearTime() is not compiling (as seen in FHNW_webeC_RoomReservation-project
-//        Date today = new Date().clearTime()
-        Date today = new Date()
+        new Project(title: "Project 123", description: "Nice project", link: "https://andiswiss.ch/project1", date: today - 365).save()
+        new Project(title: "Project 6", description: "Nice project", link: "https://andiswiss.ch/project6", date: today - 20).save()
 
-        // TODO: also the minus is not working, maybe also because of the missing .clearTime()
-//        save(new Project(title: "Project 1", description: "Nice project", link: "https://andiswiss.ch/project1", date: today - 365))
-        save(new Project(title: "Project 1", description: "Nice project", link: "https://andiswiss.ch/project1", date: today))
-        save(new Project(title: "Project 2", description: "Another project", link: "https://andiswiss.ch/project2", date: today))
-        save(new Project(title: "Project 3", description: "Yet another project", link: "https://andiswiss.ch/project3", date: today))
+        // or different way of
+        save(new Project(title: "Project 2943", description: "Another project", link: "https://andiswiss.ch/project2", date: today - 200))
+        save(new Project(title: "Project 3", description: "Yet another project", link: "https://andiswiss.ch/project3", date: today -2))
+        save(new Project(title: "Project 4", description: "Yet another project", link: "https://andiswiss.ch/project4", date: aDate))
     }
 
     static save(domainObject) {
