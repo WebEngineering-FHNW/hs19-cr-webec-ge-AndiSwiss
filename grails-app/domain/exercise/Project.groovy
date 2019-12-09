4002c975ee6526b41b97ce085e2eb18f179b0de1
Andreas Ambühl <36681000+AndiSwiss@users.noreply.github.com>
2019-12-09 11:43:00 +0100

    added BootStrap data, but I had problem with the date -> see corresponding TODO's

--- grails-app/domain/exercise/Project.groovy
+++ grails-app/domain/exercise/Project.groovy
@@ -4,11 +4,14 @@ class Project {
 
     String title
     String description
+    String link
+    Date date
 
 
     static constraints = {
         title(maxSize: 255)
         title(blank: false)
+        link(url: true)
     }
 
     @Override
--- grails-app/init/exercise/BootStrap.groovy
+++ grails-app/init/exercise/BootStrap.groovy
@@ -1,9 +1,29 @@
 package exercise
 
+import grails.util.Environment
+
 class BootStrap {
 
     def init = { servletContext ->
+        if (Environment.current == Environment.PRODUCTION) {
+            return
+        }
+
+        // TODO: .clearTime() is not compiling (as seen in FHNW_webeC_RoomReservation-project
+//        Date today = new Date().clearTime()
+        Date today = new Date()
+
+        // TODO: also the minus is not working, maybe also because of the missing .clearTime()
+//        save(new Project(title: "Project 1", description: "Nice project", link: "https://andiswiss.ch/project1", date: today - 365))
+        save(new Project(title: "Project 1", description: "Nice project", link: "https://andiswiss.ch/project1", date: today))
+        save(new Project(title: "Project 2", description: "Another project", link: "https://andiswiss.ch/project2", date: today))
+        save(new Project(title: "Project 3", description: "Yet another project", link: "https://andiswiss.ch/project3", date: today))
     }
+
+    static save(domainObject) {
+        domainObject.save(failOnError: true)
+    }
+
     def destroy = {
     }
 }
