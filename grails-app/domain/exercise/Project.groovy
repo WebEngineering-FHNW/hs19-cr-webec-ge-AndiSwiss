0288b1338ee34a0786ef61b9875df44d4d9d95bf
Andreas Ambühl <36681000+AndiSwiss@users.noreply.github.com>
2019-12-09 10:50:28 +0100

    added details to domain Project

--- grails-app/domain/exercise/Project.groovy
+++ grails-app/domain/exercise/Project.groovy
@@ -2,6 +2,17 @@ package exercise
 
 class Project {
 
+    String title
+    String description
+
+
     static constraints = {
+        title(maxSize: 255)
+        title(blank: false)
+    }
+
+    @Override
+    String toString() {
+        return "$title ($description)"
     }
 }
