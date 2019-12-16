0b3ffeba115a45945a9a6a20bc00b570220886be
Andreas Ambühl <36681000+AndiSwiss@users.noreply.github.com>
2019-12-16 10:18:20 +0100

    implemented fully custom project controller, with full CRUD implementation
    + created views for CRUD, with:  ./grailsw generate-views Project

--- grails-app/controllers/exercise/ProjectController.groovy
+++ grails-app/controllers/exercise/ProjectController.groovy
@@ -1,7 +1,108 @@
 package exercise
 
+import static org.springframework.http.HttpStatus.NOT_FOUND
+import static org.springframework.http.HttpStatus.OK
+import static org.springframework.http.HttpStatus.CREATED
+import grails.gorm.transactions.Transactional
+
 class ProjectController {
 
-    static scaffold = Project
+    static namespace = 'scaffolding'
 
+    static allowedMethods = [save: 'POST', update: 'PUT', delete: 'DELETE']
+
+    def index(Integer max) {
+        params.max = Math.min(max ?: 10, 100)
+        respond Project.list(params), model: [projectCount: Project.count()]
+    }
+
+    def show(Project project) {
+        respond project
+    }
+
+    @SuppressWarnings(['FactoryMethodName', 'GrailsMassAssignment'])
+    def create() {
+        respond new Project(params)
+    }
+
+    @Transactional
+    def save(Project project) {
+        if (project == null) {
+            transactionStatus.setRollbackOnly()
+            notFound()
+            return
+        }
+        if (project.hasErrors()) {
+            transactionStatus.setRollbackOnly()
+            respond project.errors, view: 'create'
+            return
+        }
+        project.save flush: true
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.created.message', args: [message(code: 'project.label',
+                        default: 'Project'), project.id])
+                redirect project
+            }
+            '*' { respond project, [status: CREATED] }
+        }
+    }
+
+    def edit(Project project) {
+        respond project
+    }
+
+    @Transactional
+    def update(Project project) {
+        if (project == null) {
+            transactionStatus.setRollbackOnly()
+            notFound()
+            return
+        }
+        if (project.hasErrors()) {
+            transactionStatus.setRollbackOnly()
+            respond project.errors, view: 'edit'
+            return
+        }
+        project.save flush: true
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.updated.message', args: [message(code: 'project.label',
+                        default: 'Project'), project.id])
+                redirect project
+            }
+            '*' { respond project, [status: OK] }
+        }
+    }
+
+    @Transactional
+    def delete(Project project) {
+        if (project == null) {
+            transactionStatus.setRollbackOnly()
+            notFound()
+            return
+        }
+        project.delete flush: true
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.deleted.message', args: [message(code: 'project.label',
+                        default: 'Project'), project.id])
+                redirect action: 'index', method: 'GET'
+            }
+            '*' { render status: NO_CONTENT }
+        }
+    }
+
+    protected void notFound() {
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.not.found.message', args: [message(code: 'project.label',
+                        default: 'Project'), params.id])
+            }
+        }
+    }
+
+    def fullDate() {
+        respond "1.1.1977"
+    }
 }
