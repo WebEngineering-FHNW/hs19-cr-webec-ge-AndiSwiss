b95d8525ac3a2f5f09ef9fe34e0f5af0370bc63a
Andreas Ambühl <36681000+AndiSwiss@users.noreply.github.com>
2019-12-17 22:26:55 +0100

    removed the user 'user', which was not anymore necessary, and checked, whether the app still works
    This also reflects my intended structure, since the homepage should be visible to everybody, and the BACK-END only for admins.

--- grails-app/init/exercise/BootStrap.groovy
+++ grails-app/init/exercise/BootStrap.groovy
@@ -86,20 +86,16 @@ class BootStrap {
                 cat_ui_ux: true,
                 cat_core_programming: true))
 
+
         // In production or test, this might already be in the DB
         Role adminRole = save(Role.findOrCreateWhere(authority: Role.ADMIN))
-        Role userRole = save(Role.findOrCreateWhere(authority: Role.USER))
-
-        User user = save(new User(username: 'user', password: 'user'))
         User admin = save(new User(username: 'admin', password: 'admin'))
-
-        UserRole.create(user, userRole, true)
         UserRole.create(admin, adminRole, true)
 
         // Plausibility check
-        assert Role.count() == 2
-        assert User.count() == 2
-        assert UserRole.count() == 2
+        assert Role.count() == 1
+        assert User.count() == 1
+        assert UserRole.count() == 1
 
 
     }
